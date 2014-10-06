cb1c6e6739827f78a5e07540defdbb6218870e42
Fred Hicks <sfnhmp@gmail.com>
2014-10-06 12:24:51 -0600

    v2

---
+++ Assets/Scripts/OneWayCollider.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OneWayCollider : MonoBehaviour {
+
+
+	public Collider2D colls;
+
+
+	void Start(){
+
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		if(other.tag == "Player"){
+			colls.enabled = false;
+			Debug.Log ("Collisions should be off");
+
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other){
+		if(other.tag == "Player"){
+			colls.enabled = true;
+			Debug.Log ("Collisiions should be back  on.");
+
+		}
+	}
+}
