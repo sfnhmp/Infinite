abf432be2acbaaf22b8deea154edac78e437496c
Fred Hicks <sfnhmp@gmail.com>
2014-10-18 12:41:29 -0600

    V3
    Added another building and a random tone to the buildings.

---
+++ Assets/Scripts/BuildingSpawnScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BuildingSpawnScript : MonoBehaviour {
+
+	private float randomShade = 0.0f;
+	
+	void Start(){
+		randomShade = Random.Range (0.36f, 1.0f);
+		SpriteRenderer renderer = GetComponent<SpriteRenderer> ();
+		renderer.color = new Color (randomShade, randomShade, randomShade, 1f);
+	}
+}
